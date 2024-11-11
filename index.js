ff4fed09a4434fd96cd11ee0e87fc57799a7672c
Peer Faizan <124279575+Faizan-313@users.noreply.github.com>
2024-11-11 17:02:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
+response.setHeader('Cache-Control', 'no-store');
+
 import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({extended: true}));
 
